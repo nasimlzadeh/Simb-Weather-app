aa0516fb2e9ad37ba490401347fa9175d57303f0
Nasim <nasim.lzadeh87@gmail.com>
2023-01-28 00:36:04 +0330

    added search form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function showInfo(response) {
-  console.log(response);
+  //   console.log(response);
   let tempElement = document.querySelector("#city-temp");
   tempElement.innerHTML = Math.round(response.data.main.temp);
   let humidElement = document.querySelector("#humidity");
@@ -30,20 +30,29 @@ function showInfo(response) {
   today.innerHTML = days[timestamp.getDay()];
   let hours = document.querySelector("#hours");
   let correctHour = timestamp.getHours();
-  console.log(correctHour);
   if (correctHour < 10) {
     correctHour = `0${correctHour}`;
   }
   hours.innerHTML = correctHour;
   let minutes = document.querySelector("#minutes");
   let correctMinutes = timestamp.getMinutes();
-  console.log(correctMinutes);
   if (correctMinutes < 10) {
     correctMinutes = `0${correctMinutes}`;
   }
   minutes.innerHTML = correctMinutes;
 }
-let apiKey = "d44a337dcba067ff8eff53da085f3633";
-let cityName = "tehran";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(showInfo);
+function search(city) {
+  let apiKey = "d44a337dcba067ff8eff53da085f3633";
+  let cityName = city;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showInfo);
+}
+function getCity(event) {
+  event.preventDefault();
+  let desiredCity = document.querySelector("#enter-city");
+  search(desiredCity.value);
+}
+
+let submitedCity = document.querySelector("#search-city");
+submitedCity.addEventListener("submit", getCity);
+search("tehran");
